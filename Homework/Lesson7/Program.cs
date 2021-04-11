a397e350d04bbfdee53fa57b4f1f3a22b969e2dc
Aivar Sergeev <avyshka@yandex.ru>
2021-04-11 22:02:09 +0300

    Homework 7 done

--- Homework/Lesson7/Program.cs
+++ Homework/Lesson7/Program.cs
@@ -29,8 +29,8 @@ namespace Lesson7
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
             Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
